835941f6b031c7738cdaac78d008e2e65772deee
Nate Marks <nmarks@imprivata.com>
2023-02-10 13:09:32 -0500

    cdk version and tags

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ from cdk_public_rds.cdk_public_rds_stack import RdsStack
 
 
 app = cdk.App()
+
+# tag all of the resources in the application
+cdk.Tags.of(app).add("iac", "cdk-public-rds")
+cdk.Tags.of(app).add("owner", "devops")
+cdk.Tags.of(app).add("manager", "devops")
+
 RdsStack(app, "CdkPublicRdsStack")
 
 app.synth()
